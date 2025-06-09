b89e5192097de96c2d9bc2bd6370dd9f19d7e3e1
AlmeidaNunesGabriel <dev.almeidanunesgabriel@gmail.com>
2025-06-09 12:23:29 -0300

    init

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+ 
+import Tarefas from './src/Tarefas/index';
+import Form from './src/Form';
+ 
+const Stack = createStackNavigator();
+ 
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tarefas" component={Tarefas} />
+        <Stack.Screen name="Formulario" component={Form} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
